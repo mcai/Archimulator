da32d95158f242eb0e06e3cde40d2b63e9d4d993
MIn Cai <min.cai.china@gmail.com>
2012-04-05 14:46:27 +0800

    Update auto_deploy.sh.

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,10 +1,10 @@
-rsync -avz -e "sshpass -p 1026@ustc ssh -l itecgo" ~/Archimulator/target/ itecgo@204.152.205.131:~/Archimulator/target/
+#rsync -avz -e "sshpass -p 1026@ustc ssh -l itecgo" ~/Archimulator/target/ itecgo@204.152.205.131:~/Archimulator/target/
 rsync -avz -e "sshpass -p xmttb123456 ssh -l xmttb" ~/Archimulator/target/ xmttb@110.80.31.30:~/Archimulator/target/
 
-sshpass -p "1026@ustc" ssh root@204.152.205.131 'nohup `/home/itecgo/Archimulator/run_server.sh` &'
+#sshpass -p "1026@ustc" ssh root@204.152.205.131 'nohup `/home/itecgo/Archimulator/run_server.sh` &'
 
-sshpass -p "1026@ustc" ssh root@204.152.205.131 'killall java'
-sshpass -p "1026@ustc" ssh itecgo@204.152.205.131 'nohup `/home/itecgo/Archimulator/run.sh` &'
+#sshpass -p "1026@ustc" ssh root@204.152.205.131 'killall java'
+#sshpass -p "1026@ustc" ssh itecgo@204.152.205.131 'nohup `/home/itecgo/Archimulator/run.sh` &'
 
-sshpass -p "xmttb123456" ssh xmttb@110.80.31.30 'killall java'
-sshpass -p "xmttb123456" ssh xmttb@110.80.31.30 'nohup `/home/itecgo/Archimulator/run.sh` &'
+#sshpass -p "xmttb123456" ssh xmttb@110.80.31.30 'killall java'
+#sshpass -p "xmttb123456" ssh xmttb@110.80.31.30 'nohup `/home/itecgo/Archimulator/run.sh` &'
