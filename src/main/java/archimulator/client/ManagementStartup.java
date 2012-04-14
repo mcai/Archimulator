8fee4c0e177b870c08cc433d4c43724f686a1c7b
Min Cai <itecgo@mint>
2012-04-14 17:04:39 +0800

    Minor change.

--- src/main/java/archimulator/client/ManagementStartup.java
+++ src/main/java/archimulator/client/ManagementStartup.java
@@ -196,8 +196,8 @@ public class ManagementStartup {
     //    public static final String SERVICE_URL = "http://50.117.112.114:8080/archimulator/archimulator";
     //    public static final String SERVICE_URL = "http://[2607:f358:10:13::2]:8080/archimulator/archimulator";
 //        public static final String SERVICE_URL = "http://localhost:8080/api";
-    public static final String SERVICE_URL = "http://[2607:f358:10:13::2]/api";
-//        public static final String SERVICE_URL = "http://www.archimulator.com/api";
+//    public static final String SERVICE_URL = "http://[2607:f358:10:13::2]/api";
+        public static final String SERVICE_URL = "http://www.archimulator.com/api";
 
     public static final SimulatedProgram SIMULATED_PROGRAM_MST_BASELINE = new SimulatedProgram(
             "mst_baseline", ExperimentProfile.USER_HOME_TEMPLATE_ARG + "/Archimulator/benchmarks/Olden_Custom1/mst/baseline",
