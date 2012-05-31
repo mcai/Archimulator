37bd099eeb76eea8a70c5e549e1d5c1588431401
MIn Cai <min.cai.china@gmail.com>
2012-06-01 02:11:30 +0800

    Update sync.sh.

--- sync.sh
+++ sync.sh
@@ -1,10 +1,2 @@
-#rsync -avz -e "sshpass -p 1026@ustc ssh -l itecgo" ~/Archimulator/target/ itecgo@204.152.205.131:~/Archimulator/target/
-rsync -avz -e "sshpass -p xmttb123456 ssh -l xmttb" ~/Archimulator/target/ xmttb@110.80.31.30:~/Archimulator/target/
-
-#sshpass -p "1026@ustc" ssh root@204.152.205.131 'nohup `/home/itecgo/Archimulator/run_server.sh` &'
-
-#sshpass -p "1026@ustc" ssh root@204.152.205.131 'killall java'
-#sshpass -p "1026@ustc" ssh itecgo@204.152.205.131 'nohup `/home/itecgo/Archimulator/run.sh` &'
-
-#sshpass -p "xmttb123456" ssh xmttb@110.80.31.30 'killall java'
-#sshpass -p "xmttb123456" ssh xmttb@110.80.31.30 'nohup `/home/itecgo/Archimulator/run.sh` &'
+rsync -avz -e "sshpass -p 1026@ustc ssh -l root" /home/itecgo/Archimulator_crosscompiler/ root@[2400:8900::f03c:91ff:feae:ff27]:/home/itecgo/Archimulator_crosscompiler/
+rsync -avz -e "sshpass -p 1026@ustc ssh -l root" /home/itecgo/Archimulator_crosscompiler/ root@[67.198.179.122]:/home/itecgo/Archimulator_crosscompiler/
