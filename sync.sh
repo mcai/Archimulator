f2d114a1ac210ae2f6ee3e6308eb8b951d705ed8
MIn Cai <min.cai.china@gmail.com>
2012-06-13 18:34:53 +0800

    Minor change.

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 rsync -avz -e "sshpass -p 1026@ustc ssh -l root" /home/itecgo/Archimulator/ root@[2400:8900::f03c:91ff:feae:ff27]:/home/itecgo/Archimulator/
-rsync -avz -e "sshpass -p 1026@ustc ssh -l root" /home/itecgo/Archimulator/ root@[67.198.179.122]:/home/itecgo/Archimulator/
+rsync -avz -e "sshpass -p 1026@ustc ssh -l root" /home/itecgo/Archimulator/ root@67.198.179.122:/home/itecgo/Archimulator/
 
 rsync -avz -e "sshpass -p 1026@ustc ssh -l root" /home/itecgo/Archimulator_crosscompiler/ root@[2400:8900::f03c:91ff:feae:ff27]:/home/itecgo/Archimulator_crosscompiler/
-rsync -avz -e "sshpass -p 1026@ustc ssh -l root" /home/itecgo/Archimulator_crosscompiler/ root@[67.198.179.122]:/home/itecgo/Archimulator_crosscompiler/
+rsync -avz -e "sshpass -p 1026@ustc ssh -l root" /home/itecgo/Archimulator_crosscompiler/ root@67.198.179.122:/home/itecgo/Archimulator_crosscompiler/
