9bfa3bd511e601491bf7585ba80b4277c05613a5
Min Cai <min.cai.china@gmail.com>
2012-06-13 19:02:49 +0800

    Add HT Request.mm.

---
+++ doc/HT Request.mm
@@ -0,0 +1,41 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1339584954855" ID="ID_1476783022" MODIFIED="1339585132880" TEXT="HT Request">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1339585007280" ID="ID_1164147842" MODIFIED="1339585066061" POSITION="right" TEXT="Hit">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1339585012128" ID="ID_924020513" MODIFIED="1339585066064" TEXT="Redundant MSHR">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1339585032960" ID="ID_1123544156" MODIFIED="1339585066065" TEXT="Redundant Cache">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1339585048696" ID="ID_976644099" MODIFIED="1339585066066" POSITION="right" TEXT="Miss">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1339584971536" ID="ID_585178083" MODIFIED="1339585102190" TEXT="Good">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1339584980576" ID="ID_844305542" MODIFIED="1339585066067" TEXT="Late">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1339584974792" ID="ID_1082644556" MODIFIED="1339585066068" TEXT="Bad">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1339584976480" ID="ID_859062398" MODIFIED="1339585066069" TEXT="Ugly">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1339584987240" ID="ID_1005797918" MODIFIED="1339585066069" TEXT="Early">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
