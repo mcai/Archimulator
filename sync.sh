2274ddd1507007faf2bb07d199cfdcd1c041f132
Min Cai <min.cai.china@gmail.com>
2012-09-13 15:22:26 +0800

    Update experiment results.

--- sync.sh
+++ sync.sh
@@ -7,5 +7,5 @@ rsync --recursive --progress -avz -e "sshpass -p 1026@ustc ssh -l root" /home/it
 rsync --recursive --progress -avz -e "sshpass -p 1026@ustc ssh -l root" /home/itecgo/Archimulator/target/archimulator.jar root@[2607:F180:4102:1::32:14B5]:/home/itecgo/Archimulator/target/archimulator.jar
 
 copy back experiment results:
-rsync --recursive --progress -avz -avz -e "sshpass -p 1026@ustc ssh -l root" root@[2607:F180:4102:1::32:14B5]:/home/itecgo/Archimulator/experiments/ /home/itecgo/Archimulator/experiments/
+rsync --recursive --progress -avz -e "sshpass -p 1026@ustc ssh -l root" root@[2607:F180:4102:1::32:14B5]:/home/itecgo/Archimulator/experiments/ /home/itecgo/Archimulator/experiments/
 
