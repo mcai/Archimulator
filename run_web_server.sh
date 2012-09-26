c174bff66ceefea69d1a467f028593d2e081f316
MIn Cai <min.cai.china@gmail.com>
2012-09-26 09:21:13 +0800

    Update quartz.properties and heap settings in run_web_server.sh.

--- run_web_server.sh
+++ run_web_server.sh
@@ -1,2 +1,2 @@
 cd ~/Archimulator
-java -cp target/archimulator.jar archimulator.web.ArchimulatorWebServer target/archimulator.war 8080
+java -Xms1536m -Xmx2560m -cp target/archimulator.jar archimulator.web.ArchimulatorWebServer target/archimulator.war 8080
